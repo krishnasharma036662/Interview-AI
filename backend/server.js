a103119e1c9df2b06ce4864f19d515e9476c1862
krishna sharma <ks7163464@gmail.com>
2026-06-01 19:25:55 +0530

    final commit

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,10 @@
-require('dotenv').config();
-const app = require('./src/app');
-const connectDB = require('./src/config/database.js');
-const {resume,selfDescription,jobDescription} = require('./src/services/temp.js');
-const generateInterviewReport = require('./src/services/ai.service.js');
+require("dotenv").config()
+const app = require("./src/app")
+const connectToDB = require("./src/config/database")
 
+connectToDB()
 
-connectDB();
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log(`Server is running on port ${process.env.PORT || 3000}`);
-});
+    console.log(`Server is running on port ${process.env.PORT || 3000}`)
+})
